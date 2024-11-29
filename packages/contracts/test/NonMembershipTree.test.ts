0732b505357cb10d221207874c04f7b0e443d0bf
oleh <olehmisar@gmail.com>
2024-11-30 02:41:51 +0700

    test: fix tree test

--- packages/contracts/test/NonMembershipTree.test.ts
+++ packages/contracts/test/NonMembershipTree.test.ts
@@ -1,7 +1,8 @@
 import type { Fr } from "@aztec/aztec.js";
+import type { NonMembershipTree } from "@repo/interface/src/lib/services/NonMembershipTree";
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { NonMembershipTree } from "./NonMembershipTree";
+const { tsImport } = require("tsx/esm/api"); // TODO: remove when hardhat supports ESM
 chai.use(chaiAsPromised);
 
 describe("NonMembershipTree", () => {
@@ -10,6 +11,10 @@ describe("NonMembershipTree", () => {
   let tree: NonMembershipTree;
   beforeEach(async () => {
     const { Fr } = await eval(`import("@aztec/aztec.js")`);
+    const { NonMembershipTree } = (await tsImport(
+      "@repo/interface/src/lib/services/NonMembershipTree",
+      __filename,
+    )) as typeof import("@repo/interface/src/lib/services/NonMembershipTree");
     leaves = [1, 3, 4, 8].map((x) => new Fr(BigInt(x)));
     tree = await NonMembershipTree.new(leaves, depth);
   });
