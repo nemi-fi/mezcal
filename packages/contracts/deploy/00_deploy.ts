86a34640e717b2e3b5d87a77b943a60c47443d24
oleh <olehmisar@gmail.com>
2025-02-04 05:28:48 +0100

    nuke erc20 execute

--- packages/contracts/deploy/00_deploy.ts
+++ packages/contracts/deploy/00_deploy.ts
@@ -3,7 +3,6 @@ import { DeployFunction } from "hardhat-deploy/types";
 declare module "hardhat/types/runtime" {
   interface TypedHardhatDeployNames {
     PoolERC20: "PoolERC20";
-    RouterERC20: "RouterERC20";
   }
 }
 
@@ -35,41 +34,19 @@ const deploy: DeployFunction = async ({
     "Erc20TransferVerifier",
     "erc20_transfer",
   );
-  const executeVerifier = await deployVerifier(
-    "Erc20ExecuteVerifier",
-    "erc20_execute",
-  );
   const rollupVerifier = await deployVerifier("RollupVerifier", "rollup");
 
-  const router = await typedDeployments.deploy("RouterERC20", {
-    from: deployer,
-    log: true,
-    args: [],
-  });
   const pool = await typedDeployments.deploy("PoolERC20", {
     from: deployer,
     log: true,
     args: [
-      router.address,
       shieldVerifier.address,
       unshieldVerifier.address,
       joinVerifier.address,
       transferVerifier.address,
-      executeVerifier.address,
       rollupVerifier.address,
     ],
   });
-  if (
-    (await typedDeployments.read("RouterERC20", "pool")).toLowerCase() !==
-    pool.address.toLowerCase()
-  ) {
-    await typedDeployments.execute(
-      "RouterERC20",
-      { from: deployer, log: true },
-      "initialize",
-      pool.address,
-    );
-  }
 };
 
 export default deploy;
