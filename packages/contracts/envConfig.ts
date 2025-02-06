4bd35cdc9de32c0388da813bd5a7232eb37b377d
oleh <olehmisar@gmail.com>
2025-02-06 06:13:20 +0100

    chore: add ci tests

--- packages/contracts/envConfig.ts
+++ packages/contracts/envConfig.ts
@@ -6,7 +6,10 @@ import { zPrivateKey } from "./shared/utils";
  * Parse env variables in a typesafe way.
  */
 const EnvConfigSchema = z.object({
-  DEPLOYER_PRIVATE_KEY: zPrivateKey(),
+  DEPLOYER_PRIVATE_KEY: zPrivateKey().default(
+    // random key
+    "0x845074c29c6487a92fa902e06" + "f1a1fe1ac0eb801fd6624b0a486e3c04a75721f",
+  ),
 });
 
 let envConfig: z.infer<typeof EnvConfigSchema>;
